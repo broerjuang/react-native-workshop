5a6df22ebb212ca02bd24a0345d83b4ac9fb66ca
broerjuang <juangwiantoro@gmail.com>
2018-07-13 01:38:24 +0700

    configure flow

---
+++ flow-typed/global.js
@@ -0,0 +1,28 @@
+// @flow
+/* eslint-disable no-undef */
+
+import React from "react";
+import type { StyleObj } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
+
+declare type StyleSet = StyleObj | Object | Array<StyleObj | Object>;
+
+declare type ReactNode = React.Node;
+declare type ReactEl = React.Element<*>;
+declare type Route = {
+  path: string,
+  component: ReactEl,
+  title: string,
+  baseLink?: string,
+  props?: { [key: string]: mixed },
+  search?: string
+};
+
+type ObjectOf<T> = { [key: string]: T };
+
+declare type JSONData =
+  | null
+  | string
+  | number
+  | boolean
+  | Array<JSONData>
+  | ObjectOf<JSONData>;

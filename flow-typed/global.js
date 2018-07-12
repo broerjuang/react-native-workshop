a0d97ee54cb4938b4e74823c96ef745a1ec345ea
broerjuang <juangwiantoro@gmail.com>
2018-07-13 01:44:41 +0700

    Add Lint

--- flow-typed/global.js
+++ flow-typed/global.js
@@ -1,20 +1,20 @@
 // @flow
 /* eslint-disable no-undef */
 
-import React from "react";
-import type { StyleObj } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
+import React from 'react';
+import type {StyleObj} from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
 
 declare type StyleSet = StyleObj | Object | Array<StyleObj | Object>;
 
 declare type ReactNode = React.Node;
 declare type ReactEl = React.Element<*>;
 declare type Route = {
-  path: string,
-  component: ReactEl,
-  title: string,
-  baseLink?: string,
-  props?: { [key: string]: mixed },
-  search?: string
+  path: string;
+  component: ReactEl;
+  title: string;
+  baseLink?: string;
+  props?: { [key: string]: mixed };
+  search?: string;
 };
 
 type ObjectOf<T> = { [key: string]: T };

6dc85c9700e5e669f7be262b42aa95c50319fe09
broerjuang <juangwiantoro@gmail.com>
2018-08-01 13:14:15 +0700

    test

--- App.js
+++ App.js
@@ -1,27 +1,3 @@
 // @flow
 
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
-type Props = {};
-type State = {};
-export default class App extends React.Component<Props, State> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export {default} from './src/App';

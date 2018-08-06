41cb7480cf837b8a689d6cd92179e285ad74c192
david-dss <david@dihardjasoftware.com>
2018-08-06 11:43:22 +0700

    repository list

---
+++ src/components/Repos.js
@@ -0,0 +1,94 @@
+// @flow
+import React from 'react';
+import {View, Text} from 'react-native';
+import {FontAwesome, Octicons, Entypo} from '@expo/vector-icons';
+import {DARK_GREY, FACEBOOK_BLUE} from '../general/constants/colors';
+
+type Items = {
+  id: number;
+  repo_name: string;
+  description: string;
+  star: number;
+  forked: number;
+  language: string;
+  repoType: string;
+};
+
+function listRepo(props: Items) {
+  let {id, description, star, forked, language, repoType} = props;
+  let repoName = props.repo_name;
+  return (
+    <View key={id} style={styles.container}>
+      <View style={styles.detail}>
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: 'bold',
+          }}
+        >
+          {repoName}
+        </Text>
+        <Text>{description}</Text>
+        <View style={styles.bottomDetail}>
+          <View style={styles.logoAndText}>
+            <Entypo name="star" color={DARK_GREY} style={{marginRight: 2}} />
+            <Text>{star}</Text>
+          </View>
+          <View style={styles.logoAndText}>
+            <Octicons
+              name="repo-forked"
+              color={DARK_GREY}
+              style={{marginRight: 2}}
+            />
+            <Text>{forked}</Text>
+          </View>
+          <View style={styles.logoAndText}>
+            <FontAwesome
+              name="circle"
+              color={FACEBOOK_BLUE}
+              style={{marginRight: 2}}
+            />
+            <Text>{language}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.statusRepo}>
+        {repoType === 'repo' ? (
+          <Octicons
+            name="repo"
+            color={DARK_GREY}
+            style={{marginRight: 2, fontSize: 20}}
+          />
+        ) : (
+          <Octicons
+            name="repo-forked"
+            color={DARK_GREY}
+            style={{marginRight: 2, fontSize: 20}}
+          />
+        )}
+      </View>
+    </View>
+  );
+}
+
+const styles = {
+  container: {
+    flexDirection: 'row',
+    borderWidth: 0.2,
+    padding: 6,
+    backgroundColor: 'white',
+  },
+  detail: {
+    flex: 5,
+    flexDirection: 'column',
+  },
+  bottomDetail: {flexDirection: 'row', marginRight: 15, marginVertical: 3},
+  logoAndText: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 5,
+  },
+  statusRepo: {flex: 2, alignItems: 'center', justifyContent: 'center'},
+};
+
+export default listRepo;
--- src/features/profile/screens/SettingScreen.js
+++ src/features/profile/screens/SettingScreen.js
@@ -1,17 +1,40 @@
 // @flow
 
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-
+import RepositoryScreen from '../../repository/screens/RepositoryScreen';
 class SettingScreen extends Component<{}> {
   render() {
-    return (
-      <View>
-        <Text>SettingScreen</Text>
-      </View>
-    );
+    return <RepositoryScreen {...datas} />;
   }
 }
-
-//const styles = StyleSheet.create({});
+let datas = {
+  total_count: 2570,
+  incomplete_results: false,
+  items: [
+    {
+      id: 2,
+      repo_name: 'react-navigation',
+      description: 'Advance react native state management',
+      star: 2,
+      forked: 3,
+      language: 'Java Script',
+    },
+    {
+      id: 3,
+      repo_name: 'Kodefox Tax',
+      description: 'Kodefox daily task',
+      star: 2,
+      forked: 3,
+      language: 'Java Script',
+    },
+    {
+      id: 4,
+      repo_name: 'reason',
+      description: 'Advance react native state management',
+      star: 2,
+      forked: 3,
+      language: 'Ocaml',
+    },
+  ],
+};
 export default SettingScreen;

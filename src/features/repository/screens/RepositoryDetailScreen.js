ecb061875f03c070ac55d8aff0379ff923ae372c
Johan <Me@Aviano.local>
2018-08-06 09:50:24 +0700

    get profile page

---
+++ src/features/repository/screens/RepositoryDetailScreen.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class RepositoryDetailScreen extends Component<{}> {
+  render() {
+    return (
+      <View>
+        <Text>Test</Text>
+      </View>
+    );
+  }
+}
+
+export default RepositoryDetailScreen;

b359d7bcdfc07a9034fe8155f06ab6059a7f1dd7
Astrid Tamara <astrid.tamara@hotmail.com>
2018-08-09 17:37:10 +0700

    Add all icons available in Expo

--- src/general/core-ui/Icon.js
+++ src/general/core-ui/Icon.js
@@ -1,9 +1,13 @@
 // @flow
 import React from 'react';
 import {
+  Entypo,
+  EvilIcons,
+  Feather,
+  FontAwesome,
+  Foundation,
   Ionicons,
   Octicons,
-  FontAwesome,
   MaterialIcons,
   MaterialCommunityIcons,
 } from '@expo/vector-icons';
@@ -12,18 +16,27 @@ type IconProps = {
   name: string,
   size: number,
   color: string,
-  type: 'IONICONS' | 'OCTICONS' | 'FONTAWESOME' | 'MATERIAL_ICONS' | 'MATERIAL_COMMUNITY_ICONS',
+  type: 'ENTYPO' | 'EVIL_ICONS' | 'FEATHER' | 'FONTAWESOME' | 'FOUNDATION' | 'IONICONS' | 'OCTICONS'
+    | 'MATERIAL_ICONS' | 'MATERIAL_COMMUNITY_ICONS',
 };
 
 function Icon(props: IconProps) {
   let {name, size, color, type} = props;
   switch (type) {
+    case 'ENTYPO':
+      return <Entypo name={name} size={size} color={color} />;
+    case 'EVIL_ICONS':
+      return <EvilIcons name={name} size={size} color={color} />;
+    case 'FEATHER':
+      return <Feather name={name} size={size} color={color} />;
+    case 'FONTAWESOME':
+      return <FontAwesome name={name} size={size} color={color} />;
+    case 'FOUNDATION':
+      return <Foundation name={name} size={size} color={color} />;
     case 'IONICONS':
       return <Ionicons name={name} size={size} color={color} />;
     case 'OCTICONS':
       return <Octicons name={name} size={size} color={color} />;
-    case 'FONTAWESOME':
-      return <FontAwesome name={name} size={size} color={color} />;
     case 'MATERIAL_ICONS':
       return <MaterialIcons name={name} size={size} color={color} />;
     case 'MATERIAL_COMMUNITY_ICONS':

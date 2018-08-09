364e91878b75734308dd43a48d1df77041b7a10d
david-dss <david@dihardjasoftware.com>
2018-08-09 16:56:37 +0700

    refactor component and core-ui export from index

---
+++ src/general/core-ui/index.js
@@ -0,0 +1,7 @@
+//@flow
+export {default as Button} from './Button';
+export {default as DetailsContent} from './DetailsContent';
+export {default as DetailsGroup} from './DetailsGroup';
+export {default as ParallaxButtons} from './ParallaxButtons';
+export {default as RowWith3Column} from './RowWith3Column';
+export {default as SettingButton} from './SettingButton';

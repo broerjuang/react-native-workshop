ddf468e1b3393cb6461682ac5f356566c758fe56
Astrid Tamara <astrid.tamara@hotmail.com>
2018-08-09 17:59:47 +0700

    Refactor SearchUser and SearchRepository (#25)
    * Move assets/SearchTab and renderIcon to features/search/assets
    
    * Create core-ui RepoCard
    
    * Create CoreUI Icon
    
    * Create index file for CoreUI
    
    * Refactor SearchRepository to user RepoCard imported from index
    
    * Refactor SearchUserScreen
    
    * Add all icons available in Expo
    
    * Create test for coreUI Icon
    
    * Create test for coreUI RepoCard

--- src/features/search/screens/SearchRepositoryScreen.js
+++ src/features/search/screens/SearchRepositoryScreen.js
@@ -1,14 +1,7 @@
 // @flow
-
 import React, {Component} from 'react';
-import {
-  View,
-  Text,
-  ScrollView,
-  TouchableOpacity,
-  StyleSheet,
-} from 'react-native';
-import {Ionicons, Octicons, FontAwesome} from '@expo/vector-icons';
+import {ScrollView} from 'react-native';
+import {RepoCard} from '../../../general/core-ui/index';
 
 import type {NavigationScreenProp} from 'react-navigation';
 
@@ -45,49 +38,15 @@ class SearchRepositoryScreen extends Component<Object> {
         fork: true,
       },
     ];
-
-    const {
-      list,
-      repoTitle,
-      repoIconListWrapper,
-      repoIconWrapper,
-      repoIconText,
-    } = styles;
     return (
       <ScrollView style={{flex: 1, backgroundColor: '#fff'}}>
         {repoList.map((repo, index) => {
           return (
-            <TouchableOpacity
+            <RepoCard
               key={index}
-              style={list}
+              repo={repo}
               onPress={() => this._openRepo(repo)}
-            >
-              <View>
-                <Text style={repoTitle}>{repo.fullName}</Text>
-                {repo.description !== '' ? (
-                  <Text>{repo.description}</Text>
-                ) : null}
-                <View style={repoIconListWrapper}>
-                  <View style={repoIconWrapper}>
-                    <Ionicons name="md-star" size={16} color="grey" />
-                    <Text style={repoIconText}>{repo.starsCount}</Text>
-                  </View>
-                  <View style={repoIconWrapper}>
-                    <Octicons name="repo-forked" size={16} color="grey" />
-                    <Text style={repoIconText}>{repo.forksCount}</Text>
-                  </View>
-                  <View style={repoIconWrapper}>
-                    <FontAwesome name="circle" size={16} color="#f9e03b" />
-                    <Text style={repoIconText}>{repo.language}</Text>
-                  </View>
-                </View>
-              </View>
-              {repo.fork ? (
-                <Octicons name="repo-forked" size={32} color="grey" />
-              ) : (
-                <Octicons name="repo" size={32} color="grey" />
-              )}
-            </TouchableOpacity>
+            />
           );
         })}
       </ScrollView>
@@ -98,35 +57,5 @@ class SearchRepositoryScreen extends Component<Object> {
     this.props.navigation.navigate('RepositoryScreen');
   }
 }
-
-const styles = StyleSheet.create({
-  list: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    borderTopWidth: 1,
-    borderColor: '#d6d4d4',
-    padding: 12,
-  },
-  repoTitle: {
-    fontWeight: 'bold',
-  },
-  repoIconListWrapper: {
-    display: 'flex',
-    flexDirection: 'row',
-    marginTop: 4,
-  },
-  repoIconWrapper: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginRight: 8,
-  },
-  repoIconText: {
-    fontSize: 12,
-    marginLeft: 6,
-  },
-});
 
 export default SearchRepositoryScreen;

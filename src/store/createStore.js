4e8f9757bd8ff23c8927c2fbbd4e5e7352cec459
Johan <Me@Aviano.local>
2018-08-13 14:36:45 +0700

    update LoginScreen add async storage

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,4 +1,4 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore} from 'redux';
 import _markAsRead from '../reducers/markAsReadReducer';
-import loginReducer from '../reducers/loginReducer';
-export default createStore(combineReducers({_markAsRead, loginReducer}));
+
+export default createStore(_markAsRead);

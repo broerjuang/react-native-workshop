8fae5f4d72e73e84f0d1f02dfb87f25fff141b8d
Johan <Me@Aviano.local>
2018-08-15 12:30:59 +0700

    fetchJSON not connected to Redux, instead use getToken Helper

--- src/global/helpers/fetchJSON.js
+++ src/global/helpers/fetchJSON.js
@@ -1,25 +1,10 @@
 // @flow
-import {connect} from 'react-redux';
+import getToken from './getToken';
 
 type Method = 'GET' | 'HEAD' | 'PUT' | 'DELETE' | 'PATCH' | 'POST';
 
-function checkEndpoint(endpoint: string) {
-  const rootAPI = 'api.github.com';
-  if (endpoint.includes(rootAPI)) {
-    endpoint = endpoint.slice(endpoint.indexOf(rootAPI) + rootAPI.length);
-  }
-  if (endpoint.startsWith('/')) {
-    endpoint = endpoint.slice(1);
-  }
-  return endpoint;
-}
-
-function fetchJSON(
-  endpoint: string,
-  method: Method,
-  token: string,
-): Promise<JSON> {
-  endpoint = checkEndpoint(endpoint);
+function fetchJSON(endpoint: string, method: Method): Promise<JSON> {
+  let token = getToken();
   let headers = {
     method: method,
     Authorization: `token ${token}`,
@@ -29,8 +14,4 @@ function fetchJSON(
   );
 }
 
-function mapStateToProps(state) {
-  return {token: state.loginReducer.token};
-}
-
-export default connect(mapStateToProps)(fetchJSON);
+export default fetchJSON;

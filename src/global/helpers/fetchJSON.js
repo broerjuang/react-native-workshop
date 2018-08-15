1a0f36d69e62e2b7cf5374a82cb2bde40d4452d8
Johan <Me@Aviano.local>
2018-08-15 12:30:59 +0700

    fetchJSON not connected to Redux, instead use getToken Helper

--- src/global/helpers/fetchJSON.js
+++ src/global/helpers/fetchJSON.js
@@ -1,5 +1,6 @@
 // @flow
 import getToken from './getToken';
+<<<<<<< HEAD
 import {rootAPI} from '../env';
 
 type Method = 'GET' | 'HEAD' | 'PUT' | 'DELETE' | 'PATCH' | 'POST';
@@ -9,11 +10,24 @@ function fetchJSON(
   method: Method,
   token: string = getToken(),
 ): Promise<*> {
+=======
+
+type Method = 'GET' | 'HEAD' | 'PUT' | 'DELETE' | 'PATCH' | 'POST';
+
+function fetchJSON(endpoint: string, method: Method): Promise<JSON> {
+  let token = getToken();
+>>>>>>> fetchJSON not connected to Redux, instead use getToken Helper
   let headers = {
     method: method,
     headers: {Authorization: `token ${token}`},
   };
+<<<<<<< HEAD
   return fetch(rootAPI + endpoint, headers).then((res: Response) => res.json());
+=======
+  return fetch('http://api.github.com/' + endpoint, headers).then(
+    (res: Response) => res.json(),
+  );
+>>>>>>> fetchJSON not connected to Redux, instead use getToken Helper
 }
 
 export default fetchJSON;

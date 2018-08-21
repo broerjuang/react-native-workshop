28d67ff94ed49416c2a4f422bdd596101b82f57f
Astrid Tamara <astrid.tamara@hotmail.com>
2018-08-21 15:31:44 +0700

    Fix Flow Typing

--- src/features/search/screens/SearchRepositoryScreen.js
+++ src/features/search/screens/SearchRepositoryScreen.js
@@ -1,5 +1,5 @@
 // @flow
-import React, {Component} from 'react';
+import React from 'react';
 import {ScrollView} from 'react-native';
 import {RepoCard} from '../../../global/core-ui/index';
 import {connect} from 'react-redux';
@@ -7,35 +7,35 @@ import {connect} from 'react-redux';
 import type {NavigationScreenProp} from 'react-navigation';
 
 type Repo = {
-  full_name: string,
-  description: string,
-  stargazers_count: number,
-  forks_count: number,
-  language: string,
-  fork: boolean,
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  language: string;
+  fork: boolean;
 };
 
-type Object = {
-  navigation: NavigationScreenProp<[]>,
-  repos: Array<Repo>,
+type Props = {
+  navigation: NavigationScreenProp<[]>;
+  repos: Array<Repo>;
 };
 
-function SearchRepositoryScreen(props: Object) {
-  let {repos} = props;
+function SearchRepositoryScreen(props: Props) {
+  let {repos, navigation} = props;
   return (
     <ScrollView style={{flex: 1, backgroundColor: '#fff'}}>
       {repos
         ? repos.map((repo, index) => {
-            return (
-              <RepoCard
-                key={index}
-                repo={repo}
-                onPress={() =>
-                  props.navigation.navigate('RepositoryDetailScreen')
-                }
-              />
-            );
-          })
+          return (
+            <RepoCard
+              key={index}
+              repo={repo}
+              onPress={() => {
+                navigation.navigate('RepositoryDetailScreen');
+              }}
+            />
+          );
+        })
         : null}
     </ScrollView>
   );
--- src/features/search/screens/SearchUserScreen.js
+++ src/features/search/screens/SearchUserScreen.js
@@ -1,73 +1,67 @@
 // @flow
 
-import React, {Component} from 'react';
-import {
-  Text,
-  Image,
-  TouchableOpacity,
-  ScrollView,
-  StyleSheet,
-} from 'react-native';
+import React from 'react';
+import {Text, Image, TouchableOpacity, ScrollView} from 'react-native';
 import {Icon, RowWith3Column} from '../../../global/core-ui/index';
 import {connect} from 'react-redux';
 
 import type {NavigationScreenProp} from 'react-navigation';
 
 type User = {
-  login: string,
-  avatar_url: string,
+  login: string;
+  avatar_url: string;
 };
 
-type Object = {
-  navigation: NavigationScreenProp<[]>,
-  users: Array<User>,
+type Props = {
+  navigation: NavigationScreenProp<[]>;
+  users: Array<User>;
 };
 
-function SearchUserScreen(props: Object) {
+function SearchUserScreen(props: Props) {
   const {list} = styles;
   let {users} = props;
   return (
     <ScrollView style={{flex: 1, backgroundColor: '#fff'}}>
       {users
         ? users.map((user, index) => {
-            return (
-              <TouchableOpacity
-                key={index}
-                style={list}
-                onPress={() => props.navigation.navigate('ProfileScreen')}
-              >
-                <RowWith3Column
-                  left={
-                    <Image
-                      style={{width: 32, height: 32, borderRadius: 16}}
-                      source={{uri: user.avatar_url}}
-                    />
-                  }
-                  content={<Text>{user.login}</Text>}
-                  right={
-                    <Icon
-                      name="angle-right"
-                      size={24}
-                      color="grey"
-                      type="FONTAWESOME"
-                    />
-                  }
-                />
-              </TouchableOpacity>
-            );
-          })
+          return (
+            <TouchableOpacity
+              key={index}
+              style={list}
+              onPress={() => props.navigation.navigate('ProfileScreen')}
+            >
+              <RowWith3Column
+                left={
+                  <Image
+                    style={{width: 32, height: 32, borderRadius: 16}}
+                    source={{uri: user.avatar_url}}
+                  />
+                }
+                content={<Text>{user.login}</Text>}
+                right={
+                  <Icon
+                    name="angle-right"
+                    size={24}
+                    color="grey"
+                    type="FONTAWESOME"
+                  />
+                }
+              />
+            </TouchableOpacity>
+          );
+        })
         : null}
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = {
   list: {
     borderTopWidth: 1,
     borderColor: '#d6d4d4',
     paddingVertical: 6,
   },
-});
+};
 
 function mapStateToProps(state) {
   return {

e92dbcb1e22dadc9719ade98c16e649f7a1f8e1d
deandra <deandra@yahoo.com>
2018-08-27 10:57:15 +0700

    implemented saga for notifications. both functions working

--- src/global/rootSaga.js
+++ src/global/rootSaga.js
@@ -1,9 +1,11 @@
 //@flow
 import {fork} from 'redux-saga/effects';
 import profileSaga from '../features/profile/sagas/profileSaga';
+import notificationSaga from '../features/notification/sagas/notificationSaga';
 
 function* rootSaga(): Generator<*, *, *> {
   yield fork(profileSaga);
+  yield fork(notificationSaga);
 }
 
 export default rootSaga;

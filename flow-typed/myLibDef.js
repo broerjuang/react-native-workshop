3ed57770f74041462776720ddd130ae6953be3b1
Johan <Me@Aviano.local>
2018-08-28 13:18:59 +0700

    add type ReactTestRenderer and done to async test fetchJSON

--- flow-typed/myLibDef.js
+++ flow-typed/myLibDef.js
@@ -4,6 +4,7 @@ declare function fetch<T>(
   url: string,
   object?: Object,
 ): Promise<{json: () => T; text: () => string}>;
+
 declare type ReactTestRenderer = {
   toJSON(): null | ReactTestRendererJSON;
   toTree(): null | ReactTestRendererTree;

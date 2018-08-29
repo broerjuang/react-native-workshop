ca06c58b27273e4db535eac0cb6246a2ad93e605
deandra <deandra@yahoo.com>
2018-08-29 12:00:55 +0700

    notification list grouped & add mark as read functions

---
+++ src/features/notification/sagas/notificationSaga.js
@@ -0,0 +1,85 @@
+//@flow
+import {takeLatest, put} from 'redux-saga/effects';
+import fetchJSON from '../../../global/helpers/fetchJSON';
+
+function* notificationSaga(): Generator<*, *, *> {
+  yield takeLatest('NOTIFICATION_REQUESTED', fetchNotification);
+  yield takeLatest('MARK_NOTIFICATION_AS_READ', markNotificationAsRead);
+  yield takeLatest('MARK_ALL_NOTIFICATIONS_AS_READ', markAllNotificationAsRead);
+}
+
+function* fetchNotification(): Generator<*, *, *> {
+  try {
+    let unread = yield fetchJSON(
+      `notifications?all=false`,
+      'GET',
+      'cd4bfddfacbd37cb1130ec5a8d02c851aa8e9118',
+    );
+    let participating = yield fetchJSON(
+      'notifications?participating=true&all=false',
+      'GET',
+      'cd4bfddfacbd37cb1130ec5a8d02c851aa8e9118',
+    );
+
+    let all = yield fetchJSON(
+      'notifications?all=true',
+      'GET',
+      'cd4bfddfacbd37cb1130ec5a8d02c851aa8e9118',
+    );
+
+    yield put({
+      type: 'GET_UNREAD_NOTIFICATION_SUCCESS',
+      payload: {
+        unreadNotificationData: unread,
+      },
+    });
+
+    yield put({
+      type: 'GET_PARTICIPATING_NOTIFICATION_SUCCESS',
+      payload: {
+        participatingNotificationData: participating,
+      },
+    });
+
+    yield put({
+      type: 'GET_ALL_NOTIFICATION_SUCCESS',
+      payload: {
+        allNotificationData: all,
+      },
+    });
+  } catch (error) {
+    //eslint-disable-next-line no-console
+    console.log(error);
+  }
+}
+export default notificationSaga;
+
+function* markNotificationAsRead(selectedNotificationID: *) {
+  try {
+    fetchJSON(
+      `notifications/threads/${selectedNotificationID.payload}`,
+      'PATCH',
+    );
+
+    yield put({
+      type: 'MARK_AS_READ',
+      payload: {
+        markedNotification: selectedNotificationID.payload,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function* markAllNotificationAsRead() {
+  try {
+    fetchJSON(`notifications`, 'PUT');
+    // console.log('marked as read!');
+    yield put({
+      type: 'MARK_ALL_AS_READ',
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/global/rootSaga.js
+++ src/global/rootSaga.js
@@ -2,7 +2,10 @@
 import {fork} from 'redux-saga/effects';
 import authSaga from '../features/auth/sagas';
 import profilesaga from '../features/profile/sagas/profileSaga';
+import notificationSaga from '../features/notification/sagas/notificationSaga';
+
 export default function* rootSaga(): Iterable<*> {
   yield fork(authSaga);
   yield fork(profilesaga);
+  yield fork(notificationSaga);
 }

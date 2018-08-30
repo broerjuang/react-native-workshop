978d451fa4174379dc4d7374e8bbd24b93a4c2fc
Arie Novandi <6929833+nSiggy@users.noreply.github.com>
2018-08-30 07:42:03 +0000

    Fixed flow type for profile saga (#68)

--- src/features/profile/sagas/profileSaga.js
+++ src/features/profile/sagas/profileSaga.js
@@ -1,18 +1,20 @@
+//@flow
+
 import {put, all, takeLatest, fork, call} from 'redux-saga/effects';
 import fetchJSON from '../../../global/helpers/fetchJSON';
 
 //watcher
-export default function* onPageInit() {
+export default function* onPageInit(): Generator<*, *, *> {
   yield takeLatest('ON_PAGE_MOUNT', fetchDataForPage);
 }
 
 //saga
-export function* fetchDataForPage() {
+export function* fetchDataForPage(): Generator<*, *, *> {
   try {
     yield all([
-      fork(requestAndPut, [firstRequest], firstRequestActionCreator),
-      fork(requestAndPut, [secondRequest], secondRequestActionCreator),
-      fork(requestAndPut, [thirdRequest], thirdRequestActionCreator),
+      fork(requestAndPut, firstRequest, firstRequestActionCreator),
+      fork(requestAndPut, secondRequest, secondRequestActionCreator),
+      fork(requestAndPut, thirdRequest, thirdRequestActionCreator),
     ]);
   } catch (e) {
     yield put({type: 'ON_PAGE_MOUNT_ERROR', payload: {message: e}});
@@ -21,10 +23,9 @@ export function* fetchDataForPage() {
 
 //helper
 function* requestAndPut(requestParameters, actionCreator) {
-  const result = yield call(...requestParameters);
+  const result = yield call(requestParameters);
   yield put(actionCreator(result));
 }
-
 const firstRequest = () => {
   return fetchJSON('user', 'GET');
 };

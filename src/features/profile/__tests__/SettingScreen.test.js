8623dc8ee5e18e4a4028296e92e3b78d68f69e5b
david-dss <david@dihardjasoftware.com>
2018-08-31 17:19:19 +0700

    fixin settingss testing

--- src/features/profile/__tests__/SettingScreen.test.js
+++ src/features/profile/__tests__/SettingScreen.test.js
@@ -1,12 +1,16 @@
 // @flow
 import React from 'react';
 import renderer from 'react-test-renderer';
-import SettingScreen from '../screens/SettingScreen';
+import {SettingScreen} from '../screens/SettingScreen';
 
 describe('SettingScreen', () => {
   it('should render SettingScreen corectly', () => {
     let component = renderer.create(
-      <SettingScreen navigation={{navigate: (s) => s}} />,
+      <SettingScreen
+        navigation={{navigate: (s) => (s ? true : false)}}
+        isLogin={true}
+        onSignOut={() => console.log('test')}
+      />,
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
